a4d4ffc7409a52f49bb4fd865f38e1941a1d4979
Daniel <dprevoznik@gmail.com>
2020-09-01 13:13:35 -0400

    Added formatting for submit page inputs.

--- pages/submit.js
+++ pages/submit.js
@@ -1,36 +1,31 @@
+import { useState } from "react";
 import Layout from "../components/layout";
 
 export default function Submit() {
+  let [establishment, setEstablishment] = useState("");
+  let [address, setAddress] = useState("");
   return (
     <Layout>
-      <h1>Submission Page</h1>
-      <div>
-        Establishment: <input></input>
-      </div>
-      <div>
-        Address: <input></input>
-      </div>
-      <div>
-        Image: <input></input>
-      </div>
-      <div>
-        Website: <input></input>
-      </div>
-      <div>
-        Deal: <input></input>
-      </div>
-      <div>
-        Category: <select>
-            <option>Drink</option>
-            <option>Special</option>
+      <div className="submit-page">
+        <h1>Submission</h1>
+        <h3>Establishment</h3> <input></input>
+        <h3>Address</h3>
+        <input required></input>
+        <h3>Image</h3> <input></input>
+        <h3>Website</h3>
+        <input></input>
+        <h3>Deal</h3> <input></input>
+        <h3>Category</h3>
+        <select>
+          <option>Drink</option>
+          <option>Special</option>
         </select>
-      </div>
-      <div>
-        neighborhood:{" "}
+        <h3>Neighborhood</h3>
         <select>
           <option>Flatiron District</option>
           <option>Murray Hill</option>
         </select>
+        <button>Submit</button>
       </div>
     </Layout>
   );
