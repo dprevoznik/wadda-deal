935910d5d65c2e1d4cd0971af8f35bf7d3813602
Daniel <dprevoznik@gmail.com>
2020-09-01 12:31:22 -0400

    Submission page skeleton of needed fields.

--- pages/submit.js
+++ pages/submit.js
@@ -4,6 +4,34 @@ export default function Submit() {
   return (
     <Layout>
       <h1>Submission Page</h1>
+      <div>
+        Establishment: <input></input>
+      </div>
+      <div>
+        Address: <input></input>
+      </div>
+      <div>
+        Image: <input></input>
+      </div>
+      <div>
+        Website: <input></input>
+      </div>
+      <div>
+        Deal: <input></input>
+      </div>
+      <div>
+        Category: <select>
+            <option>Drink</option>
+            <option>Special</option>
+        </select>
+      </div>
+      <div>
+        neighborhood:{" "}
+        <select>
+          <option>Flatiron District</option>
+          <option>Murray Hill</option>
+        </select>
+      </div>
     </Layout>
   );
 }
