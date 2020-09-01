3d6c045c45bfcc1c4316ec8ff70c6262d544632f
Daniel <dprevoznik@gmail.com>
2020-09-01 13:33:58 -0400

    Added react state for each field. Still need to add image loading information.

--- pages/submit.js
+++ pages/submit.js
@@ -4,24 +4,32 @@ import Layout from "../components/layout";
 export default function Submit() {
   let [establishment, setEstablishment] = useState("");
   let [address, setAddress] = useState("");
+  let [image, setImage] = useState("");
+  let [website, setWebsite] = useState("");
+  let [deal, setDeal] = useState("");
+  let [category, setCategory] = useState("Drink");
+  let [neighborhood, setNeighborhood] = useState("Flatiron District");
+
   return (
     <Layout>
       <div className="submit-page">
         <h1>Submission</h1>
-        <h3>Establishment</h3> <input></input>
+        <h3>Establishment</h3>{" "}
+        <input onChange={(e) => setEstablishment(e.target.value)}></input>
         <h3>Address</h3>
-        <input required></input>
-        <h3>Image</h3> <input></input>
+        <input onChange={(e) => setAddress(e.target.value)}></input>
+        <h3>Image</h3>{" "}
+        <input onChange={(e) => setImage(e.target.value)}></input>
         <h3>Website</h3>
-        <input></input>
-        <h3>Deal</h3> <input></input>
+        <input onChange={(e) => setWebsite(e.target.value)}></input>
+        <h3>Deal</h3> <input onChange={(e) => setDeal(e.target.value)}></input>
         <h3>Category</h3>
-        <select>
+        <select onChange={(e) => setCategory(e.target.value)}>
           <option>Drink</option>
           <option>Special</option>
         </select>
         <h3>Neighborhood</h3>
-        <select>
+        <select onChange={(e) => setNeighborhood(e.target.value)}>
           <option>Flatiron District</option>
           <option>Murray Hill</option>
         </select>
