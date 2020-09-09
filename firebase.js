e6b1bc96a2c8f67a3e1109a96d2354457775b7e2
Daniel <dprevoznik@gmail.com>
2020-09-09 12:36:16 -0400

    Added firebase config for image storage

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/storage";
+
+var firebaseConfig = {
+  apiKey: process.env.FIRE_API_KEY,
+  authDomain: "wadda-deal.firebaseapp.com",
+  databaseURL: "https://wadda-deal.firebaseio.com",
+  projectId: "wadda-deal",
+  storageBucket: "wadda-deal.appspot.com",
+  messagingSenderId: "1085278322408",
+  appId: "1:1085278322408:web:f6f8064524b771fbc7935d",
+  measurementId: "G-8YFKQFBD5Q",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const storage = firebase.storage();
+
+export { storage, firebase as default };
