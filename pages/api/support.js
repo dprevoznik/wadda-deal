79314abd672941d9e9f987d23103690deaf5e424
Daniel <dprevoznik@gmail.com>
2020-09-28 15:48:11 -0400

    Added html formatting for email body. Came up with different pieces of information I would need to collect for the support submission.

--- pages/api/support.js
+++ pages/api/support.js
@@ -1,8 +1,7 @@
 import nodemailer from "nodemailer";
 
-// {email, text}
-
 export default async function supportEmail(req, res) {
+  const { email, text, _id, deal } = req.body;
   var transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -13,14 +12,25 @@ export default async function supportEmail(req, res) {
   var mailOptions = {
     from: process.env.GMAIL_USER,
     to: process.env.GMAIL_USER,
-    subject: "Sending Email using Node.js",
-    text: "That was easy!",
+    subject: `Customer Service Request - Post ID #${_id}`,
+    html: `<h1>Customer Service Request</h1>
+            <h3>Date</h3>
+                <p>${new Date()}</p>
+            <h3>Post ID</h3>    
+                <p>#${_id}</p>
+            <h3>Deal</h3>
+                <p>${deal}</p>
+            <h3>Message</h3>
+                <p>${text}</p>
+            <h3>Contact</h3>
+                <p>${email}</p>
+    `,
   };
   transporter.sendMail(mailOptions, function mailSent(error, info) {
     if (error) {
-      res.send(error);
+      res.json(error);
     } else {
-      res.send(`Email sent: ${info.response}`);
+      res.json(`Email sent: ${info.response}`);
     }
   });
 }
