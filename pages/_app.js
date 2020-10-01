87dcfee92bccb385595eb1184985f6d1277d4ec9
Daniel <dprevoznik@gmail.com>
2020-10-01 17:09:46 -0400

    Added instagram link to reroute to instagram page. Also, added favicon so branding would be synced with the instragram page.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import "../styles/globals.scss";
 import firebase from "../firebase";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/favicon2.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
