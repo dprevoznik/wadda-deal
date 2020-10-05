9d29a7f402339fa355395a7d5d553415794a5e47
Daniel <dprevoznik@gmail.com>
2020-10-04 21:07:47 -0400

    Added better copy for about page. Also, added new font type for whole application to make it look cleaner.

--- pages/about.js
+++ pages/about.js
@@ -3,29 +3,31 @@ import AOS from "aos";
 import { useEffect } from "react";
 
 function About() {
+  // Initiate scroll library
   useEffect(() => {
     AOS.init();
   }, []);
+
   return (
     <Layout>
       <div className="about-page">
-        <h1 data-aos="zoom-in" data-aos-duration="2000">Be a better Host</h1>
+        <h1 data-aos="zoom-in" data-aos-duration="2000">
+          Be a Better Host
+        </h1>
         <h2 data-aos="fade-up" data-aos-duration="1000">
           Mission
         </h2>
         <p data-aos="fade-up" data-aos-duration="1000">
-          We have made it our mission to curate a list of the best deals for
-          those looking to have an afordable night out with friends and visitors
-          of New York City.
+          We have made it our mission to make every New Yorker the perfect host!
+          We do this by curating a list of the best deals for those looking to
+          have an afordable night out with friends and visitors.
         </p>
         <h2 data-aos="fade-up" data-aos-duration="1000">
-          How
+          Strategy
         </h2>
         <p data-aos="fade-up" data-aos-duration="1000">
-          We accomplish this goal in two ways. First, our staff goes out and
-          looks for deals to post. Second, and more importantly, our community
-          submits deals they have found and want others to enjoy. This is the
-          main way our community grows.
+          Our community is our biggest asset. They submit deals they have found
+          and want others to enjoy. This is the main way our community grows.
         </p>
       </div>
     </Layout>
